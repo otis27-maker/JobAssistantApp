4acbe4e3af321ee30daefd8bd02d0669721791e1
otis27-maker <otis.service27@gmail.com>
2026-08-04 20:24:43 -0400

    fix api key toggle

--- app.py
+++ app.py
@@ -1106,8 +1106,12 @@ with st.sidebar:
 
         # Dictionary of available models per provider
     available_models = {
-        "OpenAI": ["gpt-4o-mini", "gpt-4o", "gpt-3.5-turbo"],
-        "Anthropic": ["claude-3-haiku-20240307", "claude-3-sonnet-20240229"],
+        "OpenAI": ["gpt-4o-mini", "gpt-4o"],
+        "Anthropic": [
+            "claude-3-5-sonnet-latest", 
+            "claude-3-7-sonnet-latest", 
+            "claude-3-5-haiku-latest"
+                    ],
         "Google Gemini": ["gemini-1.5-flash", "gemini-1.5-pro"],
         "Hugging Face": ["meta-llama/Meta-Llama-3-8B-Instruct"]
     }
